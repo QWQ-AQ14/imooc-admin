6e11a54a32f6c8873eaa686afa993c3f6d088b33
xlq <1135506045@qq.com>
2022-06-14 20:52:49 +0800

    feat:登录鉴权

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 const publicRoutes = [
   {
+    path: '/',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/layout/index.vue')
+  },
+  {
     path: '/login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { createStore } from 'vuex'
 import user from './modules/user.js'
+import getters from './getters.js'
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  getters,
   modules: {
     user
   }
