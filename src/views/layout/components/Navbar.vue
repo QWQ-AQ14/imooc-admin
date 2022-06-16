4dfdfbafc16698383f5f2c1baee962643534780d
xlq <1135506045@qq.com>
2022-06-16 18:07:08 +0800

    feat(Navbar):退出登录方案_页面样式

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -1,29 +1,31 @@
 <template>
   <div class="navbar">
-    <!-- 头像 -->
-    <el-dropdown class="avatar-container" trigger="hover">
-      <div class="avatar-wrapper">
-        <!-- 头像展示 -->
-        <el-avatar
-          shape="square"
-          :size="40"
-          :src="$store.getters.userInfo.avatar"
-        ></el-avatar>
-        <!-- 设置小图标 -->
-        <i class="el-icon-s-tools"></i>
-      </div>
-      <template #dropdown>
-        <el-dropdown-menu class="user-dropdwon">
-          <router-link to="/">
-            <el-dropdown-item> 首页 </el-dropdown-item>
-          </router-link>
-          <a href="https://blog.csdn.net/qq_41863447?spm=1011.2415.3001.5343"
-            ><el-dropdown-item>博客主页</el-dropdown-item></a
-          >
-          <el-dropdown-item divided> 退出登录 </el-dropdown-item>
-        </el-dropdown-menu>
-      </template>
-    </el-dropdown>
+    <div class="right-menu">
+      <!-- 头像 -->
+      <el-dropdown class="avatar-container" trigger="hover">
+        <div class="avatar-wrapper">
+          <el-avatar
+            shape="square"
+            :size="40"
+            :src="$store.getters.userInfo.avatar"
+          ></el-avatar>
+          <i class="el-icon-s-tools"></i>
+        </div>
+        <template #dropdown>
+          <el-dropdown-menu class="user-dropdown">
+            <router-link to="/">
+              <el-dropdown-item> 首页 </el-dropdown-item>
+            </router-link>
+            <a target="_blank" href="https://blog.csdn.net/qq_41863447?spm=1011.2415.3001.5343">
+              <el-dropdown-item>博客主页</el-dropdown-item>
+            </a>
+            <el-dropdown-item divided>
+              退出登录
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
@@ -31,4 +33,29 @@
 import {} from 'vue'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.navbar{
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+  .right-menu{
+    float: right;
+    display: flex;
+    align-items: center;
+    padding-right: 16px;
+    ::v-deep .avatar-container{
+      cursor: pointer;
+      .avatar-wrapper{
+        margin-top: 5px;
+        position: relative;
+        .el-avatar{
+           --el-avatar-background-color: none;
+          margin-right: 12px;
+        }
+      }
+    }
+  }
+}
+</style>
