65d2bb33e97eb058413b336802c087899dc2006b
xlq <1135506045@qq.com>
2022-06-19 18:47:20 +0800

    feat:依据动态数据，渲染面包屑

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,17 +1,47 @@
 <template>
   <el-breadcrumb class="breadcrumb" separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
-    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-    <!-- 面包屑的最后一项 -->
-    <el-breadcrumb-item>
-      <span class="no-redirect">活动详情</span>
-    </el-breadcrumb-item>
+    <transition-group name="breadcrumb">
+      <el-breadcrumb-item v-for="item in breadcrumbData" :key="item.path">
+        <!-- 不可点击项 -->
+        <span v-if="index === breadcrumbData.length - 1" class="no-redirect">{{
+          item.meta.title
+        }}</span>
+        <!-- 可点击项 -->
+        <a v-else class="redirect" @click.prevent="onLinkClick(item)">{{
+          item.meta.title
+        }}</a>
+      </el-breadcrumb-item>
+    </transition-group>
   </el-breadcrumb>
 </template>
 
 <script setup>
-import {} from 'vue'
+import { ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+const route = useRoute()
+// 生成数组数据
+const breadcrumbData = ref([])
+const getBreadcrumbData = () => {
+  breadcrumbData.value = route.matched.filter(
+    (item) => item.meta && item.meta.title
+  )
+}
+watch(
+  route,
+  () => {
+    getBreadcrumbData()
+  },
+  {
+    immediate: true
+  }
+)
+
+// 处理点击事件
+const router = useRouter()
+const onLinkClick = (item) => {
+  // console.log(item)
+  router.push(item.path)
+}
 </script>
 
 <style lang="scss" scoped>
@@ -20,7 +50,15 @@ import {} from 'vue'
   font-size: 14px;
   line-height: 50px;
   margin-left: 8px;
+  .redirect {
+    color: #666;
+    font-weight: 600;
+  }
 
+  .redirect:hover {
+    // 将来需要进行主题替换，所以这里不去写死样式
+    color: #304156;
+  }
   ::v-deep .no-redirect {
     color: #97a8be;
     cursor: text;
