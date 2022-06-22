58b92956ab49dcb01069fd05ff14b3c247bfa3c7
xlq <1135506045@qq.com>
2022-06-22 10:45:50 +0800

    feat(局部打印):渲染缺陷详情结构

--- src/views/user-info/index.vue
+++ src/views/user-info/index.vue
@@ -1,5 +1,85 @@
 <template>
-  <div class="">用户详情</div>
+  <div class="user-info-container">
+    <el-card class="print-box">
+      <el-button type="primary">打印</el-button>
+    </el-card>
+    <el-card>
+      <div class="user-info-box">
+        <!-- 标题 -->
+        <h2 class="title">无人机巡检报告</h2>
+
+        <div class="header">
+          <!-- 头部渲染表格 -->
+          <el-descriptions :column="2" border>
+            <el-descriptions-item label="姓名">{{
+              detailData.username
+            }}</el-descriptions-item>
+            <el-descriptions-item label="性别">{{
+              detailData.gender
+            }}</el-descriptions-item>
+            <el-descriptions-item label="祖籍">{{
+              detailData.nationality
+            }}</el-descriptions-item>
+            <el-descriptions-item label="电话号码">{{
+              detailData.mobile
+            }}</el-descriptions-item>
+            <el-descriptions-item label="省份">{{
+              detailData.province
+            }}</el-descriptions-item>
+            <el-descriptions-item label="日期">{{
+              $filters.dateFilter(detailData.openTime)
+            }}</el-descriptions-item>
+            <el-descriptions-item label="备注" :span="2">
+              <el-tag
+                class="remark"
+                size="small"
+                v-for="(item, index) in detailData.remark"
+                :key="index"
+                >{{ item }}</el-tag
+              >
+            </el-descriptions-item>
+            <el-descriptions-item
+              label="地址"
+              :span="2"
+              >{{ detailData.address }}</el-descriptions-item
+            >
+          </el-descriptions>
+          <!-- 头像渲染 -->
+          <el-image
+            class="avatar"
+            :src="detailData.avatar"
+            :preview-src-list="[detailData.avatar]"
+          ></el-image>
+        </div>
+        <div class="body">
+          <!-- 内容渲染表格 -->
+          <el-descriptions direction="vertical" :column="1" border>
+            <el-descriptions-item label="经历">
+              <ul>
+                <li v-for="(item, index) in detailData.experience" :key="index">
+                  <span>
+                    {{ $filters.dateFilter(item.startTime, 'YYYY/MM') }}
+                    ----
+                    {{ $filters.dateFilter(item.endTime, 'YYYY/MM') }}</span
+                  >
+                  <span>{{ item.title }}</span>
+                  <span>{{ item.desc }}</span>
+                </li>
+              </ul>
+            </el-descriptions-item>
+            <el-descriptions-item label="专业">
+              {{ detailData.major }}
+            </el-descriptions-item>
+            <el-descriptions-item label="展览">
+              {{ detailData.glory }}
+            </el-descriptions-item>
+          </el-descriptions>
+        </div>
+        <!-- 尾部签名 -->
+        <div class="foot">签名</div>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script setup>
@@ -19,4 +99,47 @@ const getUserDetail = async () => {
 getUserDetail()
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.print-box {
+  margin-bottom: 20px;
+  text-align: right;
+}
+.user-info-box {
+  width: 1024px;
+  margin: 0 auto;
+  .title {
+    text-align: center;
+    margin-bottom: 18px;
+  }
+  .header {
+    display: flex;
+    ::v-deep .el-descriptions {
+      flex-grow: 1;
+    }
+    .avatar {
+      width: 187px;
+      box-sizing: border-box;
+      padding: 30px 20px;
+      border: 1px solid #ebeef5;
+      border-left: none;
+    }
+    .remark {
+      margin-right: 12px;
+    }
+  }
+  .body {
+    ul {
+      list-style: none;
+      li {
+        span {
+          margin-right: 62px;
+        }
+      }
+    }
+  }
+  .foot {
+    margin-top: 42px;
+    text-align: right;
+  }
+}
+</style>
