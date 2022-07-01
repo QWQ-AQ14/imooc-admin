ffcd4d83ed4a99d9b02a076096fc3d1484071a9e
xlq <1135506045@qq.com>
2022-07-01 20:18:39 +0800

    feat:虚拟列表：监听滚动事件动态截取数据

--- src/views/role-list/components/tabelList.vue
+++ src/views/role-list/components/tabelList.vue
@@ -1,6 +1,6 @@
 <template>
-  <el-scrollbar ref="scrollbarRef" height="800px" @scroll="scroll">
-    <el-table :data="tableList" border style="width: 100%">
+  <el-scrollbar ref="scrollbarRef" height="800px" @scroll="handelScroll">
+    <el-table :data="showDataList" border style="width: 100%">
       <!-- 索引 -->
       <el-table-column prop="id" label="id"> </el-table-column>
       <el-table-column prop="title" label="title"> </el-table-column>
@@ -16,7 +16,7 @@
 import axios from 'axios'
 import { ElMessage, ElScrollbar } from 'element-plus'
 
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 // 渲染基本架构
 const tableList = ref([])
 const num = ref(20)
@@ -42,12 +42,32 @@ const getMockData = (num) => {
 const srollHeight = ref(800)
 // 单个列表项的高度
 const oneHeight = ref(48)
-const cotainSize = ref(0)
+// 可视区域可以显示的列表总数
+const containSize = ref(0)
 const getContainSize = () => {
   console.log(~~(srollHeight.value / oneHeight.value))
-  cotainSize.value = ~~(srollHeight.value / oneHeight.value)
+  containSize.value = ~~(srollHeight.value / oneHeight.value)
 }
-
+// 监听滚动事件动态截取数据
+// 记录当前滚动的第一个元素的索引
+const startIndex = ref(0)
+// 定义滚动行为事件方法
+const handelScroll = ({ scrollTop }) => {
+  startIndex.value = ~~(scrollTop / oneHeight.value)
+}
+// 通过计算属性获取列表最后一个元素的索引
+const getEndIndex = computed(() => {
+  let endIndex = startIndex.value + containSize.value
+  if (!tableList.value[endIndex]) {
+    endIndex = tableList.value.length - 1
+  }
+  return endIndex
+})
+// 定义可以动态显示的数组列表元素
+const showDataList = computed(() => {
+  console.log(startIndex.value, getEndIndex.value)
+  return tableList.value.slice(startIndex.value, getEndIndex.value)
+})
 const scrollbarRef = ref()
 onMounted(() => {
   getMockData(num)
