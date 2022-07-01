a9171acef58772a1f03683fd087d33099972c150
xlq <1135506045@qq.com>
2022-07-01 20:42:46 +0800

    feat:虚拟列表：动态设置上下空白占位

--- src/views/role-list/components/tabelList.vue
+++ src/views/role-list/components/tabelList.vue
@@ -1,14 +1,16 @@
 <template>
   <el-scrollbar ref="scrollbarRef" height="800px" @scroll="handelScroll">
-    <el-table :data="showDataList" border style="width: 100%">
-      <!-- 索引 -->
-      <el-table-column prop="id" label="id"> </el-table-column>
-      <el-table-column prop="title" label="title"> </el-table-column>
-      <el-table-column prop="image" label="image"> </el-table-column>
-      <el-table-column prop="reads" label="reads"> </el-table-column>
-      <el-table-column prop="from" label="from"> </el-table-column>
-      <el-table-column prop="date" label="date"> </el-table-column>
-    </el-table>
+    <div :style="blankFillStyle">
+      <el-table :data="showDataList" border style="width: 100%">
+        <!-- 索引 -->
+        <el-table-column prop="id" label="id"> </el-table-column>
+        <el-table-column prop="title" label="title"> </el-table-column>
+        <el-table-column prop="image" label="image"> </el-table-column>
+        <el-table-column prop="reads" label="reads"> </el-table-column>
+        <el-table-column prop="from" label="from"> </el-table-column>
+        <el-table-column prop="date" label="date"> </el-table-column>
+      </el-table>
+    </div>
   </el-scrollbar>
 </template>
 
@@ -17,9 +19,9 @@ import axios from 'axios'
 import { ElMessage, ElScrollbar } from 'element-plus'
 
 import { computed, onMounted, ref } from 'vue'
-// 渲染基本架构
+// 1. 渲染基本架构
 const tableList = ref([])
-const num = ref(20)
+const num = ref(50)
 const isRequestStatus = ref(true)
 const getMockData = (num) => {
   isRequestStatus.value = true
@@ -48,7 +50,7 @@ const getContainSize = () => {
   console.log(~~(srollHeight.value / oneHeight.value))
   containSize.value = ~~(srollHeight.value / oneHeight.value)
 }
-// 监听滚动事件动态截取数据
+// 2.监听滚动事件动态截取数据
 // 记录当前滚动的第一个元素的索引
 const startIndex = ref(0)
 // 定义滚动行为事件方法
@@ -65,9 +67,24 @@ const getEndIndex = computed(() => {
 })
 // 定义可以动态显示的数组列表元素
 const showDataList = computed(() => {
-  console.log(startIndex.value, getEndIndex.value)
+  // console.log(startIndex.value, getEndIndex.value)
   return tableList.value.slice(startIndex.value, getEndIndex.value)
 })
+
+// 3.动态设置上下空白占位
+// 定义上空白的高度以及下空白填充的高度
+// const topBlankFill = computed(() => startIndex.value * oneHeight.value)
+// const bottomBlankFill = computed(
+//   () => (tableList.value.length - getEndIndex.value) * oneHeight.value
+// )
+const blankFillStyle = computed(() => {
+  return {
+    paddingTop: startIndex.value * oneHeight.value + 'px',
+    paddingBottom:
+      (tableList.value.length - getEndIndex.value) * oneHeight.value + 'px'
+  }
+})
+
 const scrollbarRef = ref()
 onMounted(() => {
   getMockData(num)
